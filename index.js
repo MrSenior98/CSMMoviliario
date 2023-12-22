64a2112b91337dfb77ef4df4909a50b0184aab6b
AlexisLuna <alexisgabriel.lu@gmail.com>
2023-12-22 11:26:48 -0500

    update pictires, update nav-bar on index

--- index.js
+++ index.js
@@ -2,5 +2,19 @@ const btnCart = document.querySelector('.container-icon')
 const containerCartProducts = document.querySelector('.container-cart-products')
 
 btnCart.addEventListener('click', () => {
-    containerCartProducts.classList.toggle('hidden-cart')
-})
+        containerCartProducts.classList.toggle('hidden-cart')
+    })
+    /* aqui comienza firebase */
+var database = firebase.database();
+var ref = database.ref('https://csmmobiliario-default-rtdb.firebaseio.com');
+/* envio de info a firebase */
+document.getElementById('Formulario').addEventListener('submit', function(event) {
+    // Previene la recarga de la página
+    event.preventDefault();
+    var datos = {
+        campo1: document.getElementById('campo1').value,
+        campo2: document.getElementById('campo2').value,
+        // ...
+    };
+    firebase.database().ref('ruta/a/tus/datos').set(datos);
+});
